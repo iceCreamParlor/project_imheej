6392df367944bc0cf4e7f9dd7db8b17c2224e67b
root <root@ip-172-31-24-61.ap-northeast-1.compute.internal>
2018-02-09 16:47:34 +0000

    first deploy

--- jan_02/settings.py
+++ jan_02/settings.py
@@ -10,12 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
-import os
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'e#zgv&+qat!hoym&$gg5j5-ta@v2pv^*p#=!(b)@_8d3-j$d^4'
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+with open(secret_file) as f:
+	secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+	try:
+		return secrets[setting]
+	except KeyError:
+		error_msg = "Set the {}environment variable".format(setting)
+		raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
