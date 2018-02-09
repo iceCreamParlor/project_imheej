74190dc8c4f26934cfe9e5a57ebfc38bbbc5e4c3
iceCreamParlor <heejjeeh@gmail.com>
2018-02-09 16:25:08 +0000

    test

--- mypage/models.py
+++ mypage/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
-        return self.title
+        return self.content
 
 
 class DiaryComment(models.Model):
